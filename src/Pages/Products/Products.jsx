58b0af963330480d64ddb0805ee2990398f2d259
Abu Naim <abunaim140@gmail.com>
2024-08-15 12:38:49 +0600

    implement get products to the database in products section

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,8 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 const Products = () => {
+    const axiosPublic = useAxiosPublic();
+
+    const { isPending: isProductLoading, data: products } = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/products');
+            return res.data;
+        }
+    });
+    console.log(products);
+
+    if(isProductLoading) {
+        return <p>Loading...</p>
+    }
+
     return (
         <div>
-            
+
         </div>
     );
 };
