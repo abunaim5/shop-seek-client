a16dd5966c53c23935358404ba757fdebd0599e8
Abu Naim <abunaim140@gmail.com>
2024-08-15 15:35:47 +0600

    added product card in product section and search input

--- src/Pages/Products/ProductCard.jsx
+++ src/Pages/Products/ProductCard.jsx
@@ -1,8 +1,22 @@
+import { PiShoppingCart } from "react-icons/pi";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
     return (
-        <div>
-            
+        <div className="card card-compact rounded-md">
+            <figure>
+                <img
+                    className='rounded-md'
+                    src={product.image}
+                    alt={`${product.name} image`} />
+            </figure>
+            <div className="card-body flex-row items-center justify-between border-2 border-gray-200 mt-4 rounded-md">
+                <div className='flex-1'>
+                    <h2 className="card-title">{product.name}</h2>
+                    <p>${product.price}</p>
+                </div>
+                <div className="divider divider-horizontal" />
+                <PiShoppingCart className='text-3xl' />
+            </div>
         </div>
     );
 };
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { CiSearch } from "react-icons/ci";
+import ProductCard from "./ProductCard";
 
 const Products = () => {
     const axiosPublic = useAxiosPublic();
@@ -13,13 +15,25 @@ const Products = () => {
     });
     console.log(products);
 
-    if(isProductLoading) {
+    if (isProductLoading) {
         return <p>Loading...</p>
     }
 
     return (
-        <div>
-
+        <div className='max-w-[1400px] mx-auto px-2'>
+            <div className='sm:w-full md:w-1/2 mx-auto my-20'>
+                <label className="input input-bordered flex items-center gap-2 rounded-md">
+                    <input type="text" className="grow" placeholder="Search" />
+                    <CiSearch />
+                </label>
+            </div>
+            <div>
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                    {
+                        products.map(product => <ProductCard key={product._id} product={product} />)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
