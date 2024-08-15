5cda27077c1567f1e1778e0663dd8dd0e9ecefb4
Abu Naim <abunaim140@gmail.com>
2024-08-16 01:42:28 +0600

    added categorization and sorting components

---
+++ src/Components/SelectOption/SelectOption.jsx
@@ -0,0 +1,15 @@
+
+const SelectOption = ({ optionsData }) => {
+    const { name, options, borderCls, roundCls, value, setValue } = optionsData;
+
+    return (
+        <select defaultValue={name} onChange={(e) => setValue(e.target.value)} className={`select select-bordered focus:outline-none rounded-md ${borderCls} ${roundCls} border-accent`}>
+            <option disabled>{name}</option>
+            {
+                options.map((option, idx) => <option key={idx} value={option}>{option}</option>)
+            }
+        </select>
+    );
+};
+
+export default SelectOption;
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -5,16 +5,19 @@ import ProductCard from "./ProductCard";
 import ReactPaginate from 'react-paginate';
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import SelectOption from "../../Components/SelectOption/SelectOption";
 
 const Products = () => {
     const axiosPublic = useAxiosPublic();
     const [products, setProducts] = useState([])
     const [itemsPerPage, setItemsPerPage] = useState(5);
     const [currentPage, setCurrentPage] = useState(0);
+    const [brandValue, setBrandValue] = useState('')
     const { count } = useLoaderData();
     const pageCount = Math.ceil(count / itemsPerPage);
     const pages = [...Array(pageCount).keys()];
 
+    // fetch products data and handle fetching data with pagination
     useEffect(() => {
         const fetchProducts = async () => {
             try {
@@ -27,31 +30,102 @@ const Products = () => {
         fetchProducts();
     }, [currentPage, itemsPerPage]);
 
+    // handle items per page count
     const handleItemsPerPage = e => {
         const pageValue = parseInt(e.target.value);
         setItemsPerPage(pageValue);
         setCurrentPage(0);
     }
 
+    // handle previous page
     const handlePrev = () => {
         if (currentPage > 0) {
             setCurrentPage(currentPage - 1);
         }
     };
 
+    // handle next page
     const handleNext = () => {
         if (currentPage < pages.length - 1) {
             setCurrentPage(currentPage + 1);
         }
     };
 
+    const brandOptions = {
+        name: 'Brand',
+        options: [
+            'All',
+            'Nike',
+            'Bata'
+        ],
+        value: brandValue,
+        setValue: setBrandValue,
+        roundCls: 'rounded-r-none',
+        borderCls: ''
+    };
+
+    const categoryOptions = {
+        name: 'Category',
+        options: [
+            'All',
+            'Nike',
+            'Bata'
+        ],
+        roundCls: 'rounded-none',
+        borderCls: 'border-x-0'
+    };
+
+    const priceOptions = {
+        name: 'Price',
+        options: [
+            'All',
+            'Nike',
+            'Bata'
+        ],
+        roundCls: 'rounded-l-none',
+        borderCls: ''
+    };
+
+    const priceSortOptions = {
+        name: 'Sort by Price',
+        options: [
+            'All',
+            'Low to High',
+            'High to Low'
+        ],
+        roundCls: 'rounded-r-none',
+        borderCls: ''
+    };
+
+    const dateSortOptions = {
+        name: 'Sort by Date',
+        options: [
+            'All',
+            'Newest',
+            'Oldest'
+        ],
+        roundCls: 'rounded-l-none',
+        borderCls: 'border-l-0'
+    };
+
     return (
         <div className='max-w-[1400px] mx-auto px-2 mb-20'>
             <div className='sm:w-full md:w-1/2 mx-auto my-20'>
-                <label className="input input-bordered flex items-center gap-2 rounded-md">
+                <label className="input focus:outline-none input-bordered flex items-center gap-2 rounded-md">
                     <input type="text" className="grow" placeholder="Search" />
                     <CiSearch />
                 </label>
+            </div>
+            <div className="flex items-center justify-between mb-8">
+                <div>
+                    <SelectOption optionsData={brandOptions} />
+                    <SelectOption optionsData={categoryOptions} />
+                    <SelectOption optionsData={priceOptions} />
+                </div>
+                <div>
+                    <SelectOption optionsData={priceSortOptions} />
+                    <SelectOption optionsData={dateSortOptions} />
+                </div>
             </div>
             <div>
                 <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
